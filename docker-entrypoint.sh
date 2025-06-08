bb39cca4e885c33a182f496e84eff76a3f0ebead
Truc Pham <kientruc582@gmail.com>
2025-06-09 00:27:00 +0700

    final, not ssh cer

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,11 +23,3 @@ nginx -t
 # Chạy nginx
 echo "Starting Nginx..."
 exec nginx -g 'daemon off;'
-
-# #!/bin/bash
-# # Thay thế giá trị trong config.js
-# sed -i "s|__VITE_APP_ROOT_API__|$VITE_APP_ROOT_API|g" /usr/share/nginx/html/config.js
-# sed -i "s|__VITE_APP_WEBSOCKET_URL__|$VITE_APP_WEBSOCKET_URL|g" /usr/share/nginx/html/config.js
-
-# # Chạy nginx
-# exec nginx -g 'daemon off;'
